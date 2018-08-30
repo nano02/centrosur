6c63f11c04f3a72eb8a666be01b977bb3739a901
Andrea <andrea.correa@utec.edu.uy>
2018-08-30 10:49:48 -0300

    Tarea 4
    Tarea 4 AppMov

---
+++ Tarea4/app/src/main/java/com/ejemplo/tarea4/MainActivity.java
@@ -0,0 +1,29 @@
+package com.ejemplo.tarea4;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity{
+
+
+    public void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void broadcastIntent(View view){
+        Intent intent = new Intent();
+        intent.setAction("com.ejemplo.tarea4");
+        sendBroadcast(intent);
+    }
+
+}
+
+
+
